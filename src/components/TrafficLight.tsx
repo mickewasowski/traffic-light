64146bab33a04a8b36d9b5ec890b21f9ed14bde8
Rosen Yorgov <yorgov.h.r@gmail.com>
2025-09-07 18:34:40 +0300

    refactored - added one more active state type, added transitions map, added lights duration map

--- src/components/TrafficLight.tsx
+++ src/components/TrafficLight.tsx
@@ -3,53 +3,43 @@ import "./TrafficLight.scss";
 
 enum Lights {
   Red,
+  RedYellow,
   Yellow,
   Green,
 }
 
-type ActiveLight = Lights.Red | Lights.Yellow | Lights.Green;
+type ActiveLight = Lights.Red | Lights.RedYellow | Lights.Yellow | Lights.Green;
 
-const DURATIONS = {
-  [Lights.Red]: {
-    light: 4_000,
-  },
-  [Lights.Yellow]: {
-    light: 1_000,
-  },
-  [Lights.Green]: {
-    light: 6_000,
-  },
+const DURATIONS: Record<ActiveLight, number> = {
+  [Lights.Red]: 4_000,
+
+  [Lights.RedYellow]: 2_000,
+
+  [Lights.Yellow]: 1_000,
+
+  [Lights.Green]: 6_000,
+};
+
+// transitions:
+// red -> red + yellow
+// red + yellow -> green
+// green -> yellow
+// yellow -> red
+const TRANSITIONS: Record<ActiveLight, ActiveLight> = {
+  [Lights.Red]: Lights.RedYellow,
+  [Lights.RedYellow]: Lights.Green,
+  [Lights.Green]: Lights.Yellow,
+  [Lights.Yellow]: Lights.Red,
 };
 
 const TrafficLight = () => {
-  const [active, setActive] = useState<ActiveLight[]>([Lights.Red]);
+  const [active, setActive] = useState<ActiveLight>(Lights.Red);
   const timeoutRef = useRef<number | null>(null);
 
-  // transitions:
-  // red -> red + yellow
-  // red + yellow -> green
-  // green -> yellow
-  // yellow -> red
   const handleTimeouts = () => {
-    if (active.length === 2) {
-      clearTimeoutRef();
-
-      timeoutRef.current = setTimeout(() => {
-        setActive([Lights.Green]);
-      }, 2_000);
-    } else if (active.includes(Lights.Green)) {
-      timeoutRef.current = setTimeout(() => {
-        setActive([Lights.Yellow]);
-      }, DURATIONS[active[0]].light);
-    } else if (active.includes(Lights.Yellow)) {
-      timeoutRef.current = setTimeout(() => {
-        setActive([Lights.Red]);
-      }, DURATIONS[active[0]].light);
-    } else if (active.includes(Lights.Red)) {
-      timeoutRef.current = setTimeout(() => {
-        setActive([Lights.Red, Lights.Yellow]);
-      }, DURATIONS[active[0]].light);
-    }
+    timeoutRef.current = setTimeout(() => {
+      setActive(TRANSITIONS[active]);
+    }, DURATIONS[active]);
   };
 
   const clearTimeoutRef = () => {
@@ -67,13 +57,13 @@ const TrafficLight = () => {
   return (
     <div className="TrafficLight">
       <div
-        className={`TrafficLight__light ${active.includes(Lights.Red) ? "TrafficLight__light--active" : ""} TrafficLight__light--red`}
+        className={`TrafficLight__light ${[Lights.Red, Lights.RedYellow].includes(active) ? "TrafficLight__light--active" : ""} TrafficLight__light--red`}
       ></div>
       <div
-        className={`TrafficLight__light ${active.includes(Lights.Yellow) ? "TrafficLight__light--active" : ""} TrafficLight__light--yellow`}
+        className={`TrafficLight__light ${[Lights.Yellow, Lights.RedYellow].includes(active) ? "TrafficLight__light--active" : ""} TrafficLight__light--yellow`}
       ></div>
       <div
-        className={`TrafficLight__light ${active.includes(Lights.Green) ? "TrafficLight__light--active" : ""} TrafficLight__light--green`}
+        className={`TrafficLight__light ${active === Lights.Green ? "TrafficLight__light--active" : ""} TrafficLight__light--green`}
       ></div>
     </div>
   );
